8e3e15bb0da6f28c2f32302288f5203d2b5ff4a3
PriyankaSaraf2001 <141492764+PriyankaSaraf2001@users.noreply.github.com>
2023-10-02 23:13:22 +0530

    Add files via upload

---
+++ src/main/java/com/example/loan_management_backend_new/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,15 @@
+package com.example.loan_management_backend_new.exceptions;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+	public EmployeeNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmployeeNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/example/loan_management_backend_new/exceptions/ErrorHandler.java
@@ -0,0 +1,38 @@
+package com.example.loan_management_backend_new.exceptions;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+
+@ControllerAdvice
+public class ErrorHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<String> handleInvalidMethodArgument(MethodArgumentNotValidException ex){
+		StringBuilder sb=new StringBuilder();
+		BindingResult bindingResult=ex.getBindingResult();
+		List<ObjectError> errors=bindingResult.getAllErrors();
+		
+		for (ObjectError objectError : errors) {
+			sb.append(objectError.getDefaultMessage()+"\n");
+		}
+		
+		return new ResponseEntity<String>(sb.toString(),HttpStatus.BAD_REQUEST);
+		
+	}
+	@ExceptionHandler(EmployeeNotFoundException.class)
+	public ResponseEntity<String> handleEmployeeNotFound(EmployeeNotFoundException ex){
+		
+		return new ResponseEntity<String>(ex.getMessage(),HttpStatus.NOT_FOUND);
+		
+	}
+}
